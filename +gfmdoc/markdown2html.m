8277dfa3758f9e66440f985ce41a80f75b9db243
David Sampson <dsampson@mathworks.com>
2020-11-24 23:29:52 +0000

    WIP handle exceptions

--- +gfmdoc/markdown2html.m
+++ +gfmdoc/markdown2html.m
@@ -15,7 +15,23 @@ switch status
     case matlab.net.http.StatusCode.Created
         html = body.html;
     otherwise
-        throwAsCaller( gitlab.exception( "create", body ) )
+        throwAsCaller( exception( "create", body ) )
 end
 
 end % markdown2html
+
+function e = exception( identifier, body )
+%exception  Create exception from identifier and response body
+%
+%  e = exception(identifier,body)
+
+if isfield( body, "error" )
+    message = body.error;
+elseif isfield( body, "message" )
+    message = body.message;
+else
+    message = "Unknown error.";
+end
+e = MException( "gitlab:" + identifier, message );
+
+end % exception
