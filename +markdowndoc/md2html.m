01bd78e7278b35051074d88ea6127bb35bfa4349
David Sampson <dsampson@mathworks.com>
2020-12-03 12:23:03 +0000

    Renamed markdown2html to md2html

--- +markdowndoc/md2html.m
+++ +markdowndoc/md2html.m
@@ -1,14 +1,21 @@
-function html = markdown2html( md )
-%markdown2html  Convert GitLab Flavored Markdown to HTML
+function html = md2html( md, hostname )
+%md2html  Convert (GitLab Flavored) Markdown to HTML
 %
-%  html = gfmdoc.markdown2html(md)
+%  html = markdowndoc.md2html(md)
+%  html = markdowndoc.md2html(md,hostname)
 
-hostname = "insidelabs-git.mathworks.com";
+% Handle inputs
+narginchk( 1, 2 )
+if nargin < 2, hostname = "insidelabs-git.mathworks.com"; end
+
+% Submit request
 data = struct( "text", md, "gfm", true );
 method = matlab.net.http.RequestMethod.POST;
 request = matlab.net.http.RequestMessage( method, [], data );
 uri = matlab.net.URI( "https://" + hostname + "/api/v4/markdown" );
 response = request.send( uri );
+
+% Handle response
 switch response.StatusCode
     case matlab.net.http.StatusCode.Created
         html = response.Body.Data.html;
@@ -16,4 +23,4 @@ switch response.StatusCode
         throw( MException( "gitlab:create", response.Body.Data.error ) )
 end
 
-end % markdown2html
+end % md2html
