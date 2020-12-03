f33b437f69cb2b8438c37e598681ed5522171e6b
David Sampson <dsampson@mathworks.com>
2020-12-03 12:12:12 +0000

    Doc

--- +gfmdoc/xslt.m
+++ +gfmdoc/xslt.m
@@ -1,8 +1,13 @@
 function xslt( source, style, destination )
+%xslt  Transform an XML document using an XSLT engine
 
-NET.addAssembly( "System.Xml" );
-xform = System.Xml.Xsl.XslCompiledTransform();
-xform.Load( style );
-xform.Transform( source, destination );
+if ispc() % .NET, fast
+    NET.addAssembly( "System.Xml" );
+    xform = System.Xml.Xsl.XslCompiledTransform();
+    xform.Load( style );
+    xform.Transform( source, destination );
+else % Java, slow
+    xslt( source, style, destination );
+end
 
 end % xslt
