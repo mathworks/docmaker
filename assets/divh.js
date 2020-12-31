056b73fc6d00dc588c349db05cc4c8c7676c1cfe
David Sampson <dsampson@mathworks.com>
2020-12-31 01:42:54 +0000

    WIP divh.js

--- assets/divh.js
+++ assets/divh.js
@@ -38,6 +38,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     main.appendChild(div);
   }
+  var collapsible = document.getElementsByClassName("collapsible");
+  for (let i = 0; i < collapsible.length; i++) {
+    collapsible[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
 })
 
 function printElements(elements) {
