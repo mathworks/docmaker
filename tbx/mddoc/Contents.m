d4467417d3d838ae80014a3e2e9663e5ff6eadbc
David Sampson <dsampson@mathworks.com>
2021-01-01 18:09:25 +0000

    Updated Contents.m

--- tbx/mddoc/Contents.m
+++ tbx/mddoc/Contents.m
@@ -1,7 +1,8 @@
-% markdowndoc
+% Markdown Documentation Toolbox
 % Version 0.1 (R2020b) 1-January-2021
 %
-% markdowndoc.publish   - publish Markdown files with stylesheets and scripts
-% markdowndoc.unpublish - unpublish Markdown files
+% mddoc   - publish Markdown files with stylesheets and scripts
+% mdundoc - unpublish Markdown files
+% mdtoc   - generate helptoc.xml
 
 %  Copyright 2020-2021 The MathWorks, Inc.
