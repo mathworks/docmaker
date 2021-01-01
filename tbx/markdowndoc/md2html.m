1812e08480c01bc10c62b428edf9e329bd9f6721
David Sampson <dsampson@mathworks.com>
2021-01-01 17:48:37 +0000

    Moved md2html from package

--- tbx/markdowndoc/md2html.m
+++ tbx/markdowndoc/md2html.m
@@ -1,5 +1,34 @@
-function varargout = md2html( varargin )
+function html = md2html( md, hostname )
+%md2html  Convert (GitLab Flavored) Markdown to HTML fragment
+%
+%  html = md2html(md) converts the Markdown md to HTML html using the
+%  GitLab API at gitlab.com.
+%
+%  html = md2html(md,hostname) uses the specified hostname.
+%
+%  MarkDown API documentation: https://docs.gitlab.com/ee/api/markdown.html
+%
+%  See also: mddoc
 
-[varargout{1:nargout}] = markdowndoc.md2html( varargin{:} );
+%  Copyright 2020-2021 The MathWorks, Inc.
 
+% Handle inputs
+narginchk( 1, 2 )
+if nargin < 2, hostname = "gitlab.com"; end
+
+% Submit request
+data = struct( "text", md, "gfm", true );
+method = matlab.net.http.RequestMethod.POST;
+request = matlab.net.http.RequestMessage( method, [], data );
+uri = matlab.net.URI( "https://" + hostname + "/api/v4/markdown" );
+response = request.send( uri );
+
+% Handle response
+switch response.StatusCode
+    case matlab.net.http.StatusCode.Created
+        html = response.Body.Data.html;
+    otherwise
+        throw( MException( "gitlab:create", response.Body.Data.error ) )
 end
+
+end % md2html
