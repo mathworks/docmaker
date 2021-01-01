1aa8d2d51575c2853dd135f38343e707f580ffc2
David Sampson <dsampson@mathworks.com>
2021-01-01 11:22:07 +0000

    Reintroduced filesep to relpath

--- tbx/markdowndoc/+markdowndoc/relpath.m
+++ tbx/markdowndoc/+markdowndoc/relpath.m
@@ -1,4 +1,4 @@
-function rTo = relpath( pFrom, fTo )
+function rTo = relpath( pFr, fTo )
 %relpath  Compute relative path between two files
 %
 %  r = relpath(f,t) computes the relative path from the folder f to the
@@ -12,42 +12,21 @@ function rTo = relpath( pFrom, fTo )
 
 %  Copyright 2020-2021 The MathWorks, Inc.
 
-sFrom = separator( pFrom ); % separator
-fFrom = [pFrom, sFrom, '.']; % fullfile
-pShared = markdowndoc.superdir( {fFrom fTo} ); % common ancestor folder
-if isempty( pShared ) % no common ancestor
+fFr = fullfile( pFr, '.' );
+pSu = markdowndoc.superdir( fFr, fTo ); % common ancestor folder
+if isempty( pSu ) % no common ancestor
     rTo = fTo; % return absolute path
 else % common ancestor
-    sTo = separator( fTo ); % identify separator
     rTo = '.'; % initialize
-    while ~strcmp( pFrom, pShared )
-        rTo = [rTo, sTo, '..']; %#ok<AGROW> % append ..
-        pFrom = fileparts( pFrom ); % up one level
+    while ~strcmp( pFr, pSu )
+        rTo = fullfile( rTo, '..' );
+        pFr = fileparts( pFr ); % up one level
     end
-    if strcmp( pShared, fileparts( pShared ) ) % root, ends with separator
-        rTo = [rTo, sTo, fTo(numel(pShared)+1:end)];
+    if strcmp( pSu, fileparts( pSu ) ) % root, includes separator
+        rTo = fullfile( rTo, fTo(numel(pSu)+1:end) );
     else % not root
-        rTo = [rTo, fTo(numel(pShared)+1:end)];
+        rTo = [rTo, fTo(numel(pSu)+1:end)];
     end
 end
 
 end % relpath
-
-function s = separator( f )
-%separator  Identify file separator in filename
-%
-%  s = separator(f) identifies the file separator in the filename f by
-%  searching for, in order: filesep, /, \.  If no separator is found then
-%  filesep is assumed.
-
-if any( f == filesep )
-    s = filesep;
-elseif any( f == '/' )
-    s = '/';
-elseif any( f == '\' )
-    s = '\';
-else
-    s = filesep;
-end
-
-end % separator
