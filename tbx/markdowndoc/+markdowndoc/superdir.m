f8d8f2ebf97d31cdbafc91aaa057fc10c4a9a568
David Sampson <dsampson@mathworks.com>
2021-01-01 10:09:09 +0000

    Renamed ancestordir to superdir

--- tbx/markdowndoc/+markdowndoc/superdir.m
+++ tbx/markdowndoc/+markdowndoc/superdir.m
@@ -1,11 +1,11 @@
 function a = superdir( f )
-%ancestordir  Find common ancestor folder
+%superdir  Find lowest common superdirectory
 %
-%  r = ancestordir(f) finds a common ancestor folder for the file list f.
-%  f can be specified as a char or string, a cellstr or string array, or a
-%  dir struct.
+%  r = superdir(f) finds the lowest common superdirectory for the file list
+%  f. f can be specified as a char or string, a cellstr or string array, or
+%  a dir struct.
 %
-%  If f is empty, or if the elements of f have no common ancestor folder,
+%  If f is empty, or if the elements of f have no common superdirectory,
 %  then [] is returned.
 
 %  Copyright 2020-2021 The MathWorks, Inc.
@@ -32,4 +32,4 @@ else % normal
     end
 end
 
-end % ancestordir
+end % superdir
