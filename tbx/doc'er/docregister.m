79535a456b74a5b4381e2ee7aee64a2704e93088
David Sampson <dsampson@mathworks.com>
2024-07-12 17:44:55 +0100

    WIP info.xml in docregister

--- tbx/doc'er/docregister.m
+++ tbx/doc'er/docregister.m
@@ -1,7 +1,8 @@
-function docregister( pRoot )
-%docregister  Create helptoc XML from Markdown table of contents
+function docregister( pRoot, id )
+%docregister  Create info.xml, helptoc.xml from Contents.m, helptoc.md
 %
-%  docregister(md,xml)
+%  docregister(f,id) creates info.xml and helptoc.xml for the product with
+%  identifier id in the folder f.
 %
 %  See also: docpublish, docdemo, undoc
 
@@ -9,6 +10,7 @@ function docregister( pRoot )
 
 arguments
     pRoot (1,1) string {mustBeFolder}
+    id (1,1) string
 end
 
 % Find style sheet
@@ -18,8 +20,11 @@ fXsl = fullfile( fileparts( mfilename( "fullpath" ) ), "resources", ...
 % Put XML next to Markdown
 fMd = fullfile( pRoot, "helptoc.md" );
 fXml = fullfile( pRoot, "helptoc.xml" );
+fInfo = fullfile( pRoot, "info.xml" );
 
 % Convert to HTML and write to file
+assert( exist( fMd, "file" ), "docer:NotFound", ...
+    "Cannot find table of contents ""%s"".", fMd )
 cHtml = md2html( fileread( fMd ) ); % content
 [~, nHtml] = fileparts( tempname( pRoot ) ); % temp
 fHtml = fullfile( pRoot, nHtml + ".html" );
@@ -39,4 +44,35 @@ catch e
     rethrow( e )
 end
 
+cInfo = info( id, pRoot );
+fprintf( 1, "%s\n", cInfo );
+
 end % doctoc
+
+function c = info( id, pHelp )
+%info  Generate info.xml from product identifier and documentation folder
+
+s = ver( id ); % read metadata from Contents.m
+[~, nHelp] = fileparts( pHelp ); % just last part
+release = string( s.Release );
+if startsWith( release, "(" ) && endsWith( release, ")" )
+    release = extractBetween( release, 2, strlength( release ) - 1 );
+end
+name = string( s.Name );
+if endsWith( name, " Toolbox" )
+    name = extractBefore( name, strlength( name ) - strlength( "Toolbox" ) );
+end
+
+c(1) = "<productinfo xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xsi:noNamespaceSchemaLocation=""optional"">";
+c(2) = "<?xml-stylesheet type=""text/xsl"" href=""optional""?>";
+c(3) = sprintf( "<matlabrelease>%s</matlabrelease>", release );
+c(4) = sprintf( "<name>%s</name>", name );
+c(5) = "<type>toolbox</type>";
+c(6) = "<icon>$toolbox/matlab/icons/bookicon.gif</icon>";
+c(7) = sprintf( "<help_location>../%s</help_location>", nHelp );
+c(8) = "</productinfo>";
+
+% Combine
+c = strjoin( c, newline );
+
+end % info
