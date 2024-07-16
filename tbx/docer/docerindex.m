fbd24624ceb78afd6d7df81e017c6e23c511a510
David Sampson <dsampson@mathworks.com>
2024-07-16 20:57:25 +0100

    WIP docerindex

--- tbx/docer/docerindex.m
+++ tbx/docer/docerindex.m
@@ -17,13 +17,13 @@ sRoot = dirstruct( pRoot );
 pRoot = sRoot(1).folder;
 
 % Read helptoc.md
-fHelp = fullfile( pRoot, "helptoc.md" );
-mHelp = fileread( fHelp );
-xHelp = md2xml( mHelp );
-linkrep( xHelp, ".md", ".html" )
+fToc = fullfile( pRoot, "helptoc.md" );
+mToc = fileread( fToc );
+xToc = md2xml( mToc );
+linkrep( xToc, ".md", ".html" )
 
 % Extract name
-h1 = xHelp.getElementsByTagName( "h1" );
+h1 = xToc.getElementsByTagName( "h1" );
 if h1.Length > 0
     name = h1.item( 0 ).TextContent;
 else
@@ -38,51 +38,14 @@ w.Configuration.FormatPrettyPrint = true;
 w.writeToFile( xInfo, fInfo )
 fprintf( 1, "[+] %s\n", fInfo );
 
-return
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% Find resources folder
-pRez = fullfile( fileparts( mfilename( "fullpath" ) ), "resources" );
-
-% Create helptoc.xml from helptoc.md
-fHelpMd = fullfile( pRoot, "helptoc.md" ); % source file
-xHelp = md2html( fileread( fHelpMd ) ); % convert to XML fragment
-xHelp = "<?xml version=""1.0"" encoding=""utf-8""?>" + ...
-    "<xml>" + newline + xHelp + "</xml>"; % wrap fragment
-fHelpIn = tempname() + ".xml"; % temp file
-writelines( xHelp, fHelpIn ) % write to file
-cuHelp = onCleanup( @()delete( fHelpIn ) ); % clean up
-fHelpXsl = fullfile( pRez, "helptoc.xsl" );
-fHelpOut = fullfile( pRoot, "helptoc.xml" );
-xslt( fHelpIn, fHelpXsl, fHelpOut ); % transform
-fprintf( 1, "[+] %s\n", fHelpOut ); % echo
-
-% Create info.xml from Contents.m
-r = matlabRelease().Release;
-n = "Package Jockey";
-sInfo = struct( "release", r, "name", n );
-fInfoIn = tempname() + ".xml"; % temp file
-writestruct( sInfo, fInfoIn ); % write to file
-cuInfo = onCleanup( @()delete( fInfoIn ) ); % clean up
-fInfoXsl = fullfile( pRez, "info.xsl" );
-fInfoOut = fullfile( pRoot, "info.xml" );
-xslt( fInfoIn, fInfoXsl, fInfoOut ); % transform
-fprintf( 1, "[+] %s\n", fInfoOut ); % echo
+% Write helptoc.xml
+transformer = matlab.io.xml.transform.Transformer();
+oToc = matlab.io.xml.transform.SourceDocument( xToc );
+fXsl = "tbx/docer/resources/helptoc.xsl";
+oXsl = matlab.io.xml.transform.StylesheetSourceFile( fXsl );
+fHelp = fullfile( pRoot, "helptoc.xml" );
+oHelp = matlab.io.xml.transform.ResultFile( fHelp );
+transform( transformer, oToc, oXsl, oHelp );
+fprintf( 1, "[+] %s\n", fHelp );
 
 end % docerindex
