f1564b1d7f8b3e905b5e8857d79d7985c4ab0591
David Sampson <dsampson@mathworks.com>
2024-07-16 15:40:35 +0100

    Updated Contents.m

--- tbx/docer/Contents.m
+++ tbx/docer/Contents.m
@@ -1,10 +1,10 @@
 % Doc'er
 % Version 0.2 (R2024a) 10-Jul-2024
 %
-% docerpub - publish Markdown files to HTML with stylesheets and scripts
-% docerrun - run demo scripts and publish output figures to PNG
-% docerreg - register toolbox documentation
-% undocer  - remove Doc'er artifacts
-% md2html  - convert Markdown to HTML with GitHub.com
+% docerconvert - convert Markdown files to HTML with stylesheets and scripts
+% docerrun     - run demo scripts and publish output figures to PNG
+% docerindex   - create toolbox documentation index files
+% docerdelete  - delete Doc'er artifacts
+% docer        - Doc'er gateway function
 
 %  Copyright 2020-2024 The MathWorks, Inc.
