ea5814ed100babc81d659800f977126b608562be
David Sampson <dsampson@mathworks.com>
2024-07-30 17:31:48 -0500

    docer.run comments

--- tbx/docer/+docer/run.m
+++ tbx/docer/+docer/run.m
@@ -2,67 +2,96 @@ function run( filename )
 
 % Capture initial figures
 oldFigures = docer.figures();
-
-% Schedule cleanup
 cu = onCleanup( @()delete( setdiff( docer.figures(), oldFigures ) ) );
 
 % Create workspace
 w = docer.Workspace();
 
-% Parse document
+% Read from file
 parser = matlab.io.xml.dom.Parser();
 parser.Configuration.AllowDoctype = true;
 doc = parser.parseFile( filename );
 
-% Process divs
+% Find all divs
 divs = doc.getElementsByTagName( "div" );
 divs = list2array( divs );
+
+% Process divs
 for ii = 1:numel( divs )
+
     div = divs( ii );
-    if div.hasAttribute( "class" ) && contains( ...
+    next = div.getNextSibling();
+
+    if div.hasAttribute( "class" ) && contains( ... % MATLAB input
             div.getAttribute( "class" ), "highlight-source-matlab" )
-        inDiv = div; % code block
-        inString = div.TextContent; % extract text
-        [outString, outFigs] = docer.eval( w, inString ); % evaluate and capture
-        for jj = 1:numel( outFigs )
-            outFig = outFigs(jj);
-            outDiv = doc.createElement( "div" );
-            outDiv.setAttribute( "class", "highlight highlight-output-matlab" );
-            outImg = doc.createElement( "img" );
-            outImg.setAttribute( "src", "data:image/png;base64, " + ...
-                docer.encode( outFig ) );
-            outDiv.appendChild( outImg );
-            if isempty( inDiv.getNextSibling() )
-                inDiv.getParentNode().appendChild( outDiv );
-            else
-                inDiv.getParentNode().insertBefore( ...
-                    outDiv, inDiv.getNextSibling() );
-            end
-        end
+
+        % Extract code
+        inDiv = div;
+        inString = div.TextContent;
+
+        % Evaluate code and capture output
+        [outString, outFigs] = docer.eval( w, inString );
+
+        % Add text output
         if strlength( outString ) > 0
-            outString = textcontent( outString ); %
+
+            % Strip out markup
+            outString = textcontent( outString );
+
+            % Create HTML elements div, pre, text
             outDiv = doc.createElement( "div" );
             outDiv.setAttribute( "class", "highlight highlight-output-matlab" );
             outPre = doc.createElement( "pre" );
             outDiv.appendChild( outPre );
             outText = doc.createTextNode( outString );
             outPre.appendChild( outText );
-            if isempty( inDiv.getNextSibling() )
-                inDiv.getParentNode().appendChild( outDiv );
+
+            % Add output to document
+            if isempty( next )
+                inDiv.getParentNode().appendChild( outDiv ); % end
             else
-                inDiv.getParentNode().insertBefore( ...
-                    outDiv, inDiv.getNextSibling() );
+                inDiv.getParentNode().insertBefore( outDiv, next );
             end
+
         end
-    elseif div.hasAttribute( "class" ) && contains( ...
+
+        % Add figure output
+        for jj = 1:numel( outFigs )
+
+            outFig = outFigs(jj);
+
+            % Create HTML elements div, img
+            outDiv = doc.createElement( "div" );
+            outDiv.setAttribute( "class", "highlight highlight-output-matlab" );
+            outImg = doc.createElement( "img" );
+            outImg.setAttribute( "src", "data:image/png;base64, " + ...
+                docer.encode( outFig ) );
+            outDiv.appendChild( outImg );
+
+            % Add output to document
+            if isempty( next )
+                inDiv.getParentNode().appendChild( outDiv ); % end
+            else
+                inDiv.getParentNode().insertBefore( outDiv, next );
+            end
+
+        end
+
+    elseif div.hasAttribute( "class" ) && contains( ... % MATLAB output
             div.getAttribute( "class" ), "highlight-output-matlab" )
+
+        % Delete
         div.getParentNode().removeChild( div );
+
     end
+
 end
+
+% Write to file
 writer = matlab.io.xml.dom.DOMWriter();
 writer.writeToFile( doc, filename );
 
-end
+end % run
 
 function t = textcontent( s )
 
