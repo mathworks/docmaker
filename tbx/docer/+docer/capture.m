ab14fb81f1ea6dd2441e386bed9d70ca060fafc3
David Sampson <dsampson@mathworks.com>
2024-07-30 13:20:22 -0500

    Hide axes toolbar during figure capture

--- tbx/docer/+docer/capture.m
+++ tbx/docer/+docer/capture.m
@@ -9,6 +9,20 @@ arguments
     fig (1,1) matlab.ui.Figure
 end
 
+% Hide axes toolbars
+ax = findobj( fig, "Type", "axes" );
+tf = true( size( ax ) );
+for ii = 1:numel( ax )
+    tf(ii) = ax(ii).Toolbar.Visible;
+    ax(ii).Toolbar.Visible = false;
+end
+
+% Capture
 [x, ~] = getframe( fig );
+
+% Restore axes toolbars
+for ii = 1:numel( ax )
+    ax(ii).Toolbar.Visible = tf(ii);
+end
 
 end % capture
