a9e2427c6272eaa983d85459272c95d309bb61e3
David Sampson <dsampson@mathworks.com>
2024-07-31 18:35:42 -0500

    Reordered docerrun subfunctions

--- tbx/docer/docerrun.m
+++ tbx/docer/docerrun.m
@@ -142,78 +142,6 @@ writer.writeToFile( doc, html );
 
 end % run
 
-function nextHeading = getNextHeading( e, allHeadings )
-%getNextHeading  Get next heading
-%
-%   h = getNextHeading(e,ah) returns the next heading h from among all
-%   headings ah after the element e.
-
-nextHeading = [];
-for ii = 1:numel( allHeadings )
-    thisHeadings = allHeadings{ii};
-    for jj = 1:numel( thisHeadings )
-        thisHeading = thisHeadings(jj);
-        if isAfter( thisHeading, e ) && ( isequal( nextHeading, [] ) || ...
-                isAfter( nextHeading, thisHeading ) )
-            nextHeading = thisHeading;
-            break
-        end
-    end
-end
-
-end % getNextHeading
-
-function tf = isAfter( a, b )
-%isAfter  True if an element is after another
-%
-%   tf = isAfter(a,b) is true if a is after b, and false otherwise.
-%
-%   https://www.w3schools.com/jsref/met_node_comparedocumentposition.asp
-
-[a, b] = expand( a, b ); % expand scalars
-tf = true( size( a ) ); % preallocate
-for ii = 1:numel( a )
-    c = uint8( compareDocumentPosition( b(ii), a(ii) ) );
-    tf(ii) = bitget( c, 1 ) == false && bitget( c, 3 ) == true;
-end
-
-end % isAfter
-
-function tf = isBetween( a, b, c )
-%isBetween  True if an element is between two others
-%
-%   tf = isBetween(a,b,c) is true if a is between b and c, and false
-%   otherwise.
-
-[a, b, c] = expand( a, b, c ); % expand scalars
-tf = isAfter( a, b ) & isAfter( c, a );
-
-end % isBetween
-
-function varargout = expand( varargin )
-%expand  Perform scalar expansion
-%
-%   [a,b] = expand(a,b) expands scalar a to match the size of nonscalar b,
-%   or vice versa.
-%
-%   [a,b,c,...] = expand(a,b,c,...) performs scalar expansion on as many
-%   variables as requested.
-
-for ii = 1:nargin
-    for jj = ii+1:nargin
-        if isscalar( varargin{ii} ) && ~isscalar( varargin{jj} )
-            varargin{ii} = repmat( varargin{ii}, size( varargin{jj} ) );
-        elseif ~isscalar( varargin{ii} ) && isscalar( varargin{jj} )
-            varargin{jj} = repmat( varargin{jj}, size( varargin{ii} ) );
-        else
-            assert( isequal( size( varargin{ii} ), size( varargin{jj} ) ) )
-        end
-    end
-end
-varargout = varargin; % return; varargin is not a valid output
-
-end % expand
-
 function runDiv( div, w )
 %runDiv  Run MATLAB code from a div and insert output
 %
@@ -306,3 +234,75 @@ for jj = 1:numel( outFigures )
 end
 
 end % runDiv
+
+function nextHeading = getNextHeading( e, allHeadings )
+%getNextHeading  Get next heading
+%
+%   h = getNextHeading(e,ah) returns the next heading h from among all
+%   headings ah after the element e.
+
+nextHeading = [];
+for ii = 1:numel( allHeadings )
+    thisHeadings = allHeadings{ii};
+    for jj = 1:numel( thisHeadings )
+        thisHeading = thisHeadings(jj);
+        if isAfter( thisHeading, e ) && ( isequal( nextHeading, [] ) || ...
+                isAfter( nextHeading, thisHeading ) )
+            nextHeading = thisHeading;
+            break
+        end
+    end
+end
+
+end % getNextHeading
+
+function tf = isAfter( a, b )
+%isAfter  True if an element is after another
+%
+%   tf = isAfter(a,b) is true if a is after b, and false otherwise.
+%
+%   https://www.w3schools.com/jsref/met_node_comparedocumentposition.asp
+
+[a, b] = expand( a, b ); % expand scalars
+tf = true( size( a ) ); % preallocate
+for ii = 1:numel( a )
+    c = uint8( compareDocumentPosition( b(ii), a(ii) ) );
+    tf(ii) = bitget( c, 1 ) == false && bitget( c, 3 ) == true;
+end
+
+end % isAfter
+
+function tf = isBetween( a, b, c )
+%isBetween  True if an element is between two others
+%
+%   tf = isBetween(a,b,c) is true if a is between b and c, and false
+%   otherwise.
+
+[a, b, c] = expand( a, b, c ); % expand scalars
+tf = isAfter( a, b ) & isAfter( c, a );
+
+end % isBetween
+
+function varargout = expand( varargin )
+%expand  Perform scalar expansion
+%
+%   [a,b] = expand(a,b) expands scalar a to match the size of nonscalar b,
+%   or vice versa.
+%
+%   [a,b,c,...] = expand(a,b,c,...) performs scalar expansion on as many
+%   variables as requested.
+
+for ii = 1:nargin
+    for jj = ii+1:nargin
+        if isscalar( varargin{ii} ) && ~isscalar( varargin{jj} )
+            varargin{ii} = repmat( varargin{ii}, size( varargin{jj} ) );
+        elseif ~isscalar( varargin{ii} ) && isscalar( varargin{jj} )
+            varargin{jj} = repmat( varargin{jj}, size( varargin{ii} ) );
+        else
+            assert( isequal( size( varargin{ii} ), size( varargin{jj} ) ) )
+        end
+    end
+end
+varargout = varargin; % return; varargin is not a valid output
+
+end % expand
