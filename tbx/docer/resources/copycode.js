a0b6dd3a4e13d677982da43f8a3e1815b2067847
David Sampson <dsampson@mathworks.com>
2024-09-19 17:25:20 +0100

    WIP js copy button

--- tbx/docer/resources/copycode.js
+++ tbx/docer/resources/copycode.js
@@ -1,14 +1,14 @@
-document.querySelectorAll('.highlight-source-matlab pre button').forEach(button => {
-    pre = button.parentNode;
+document.querySelectorAll('.highlight-source-matlab pre').forEach(pre => {
+    const button = document.createElement('button');
+    button.className = 'copy-button';
+    pre.appendChild(button);
     button.addEventListener('click', async () => {
         try {
-            const originalText = button.textContent;
-            await navigator.clipboard.writeText(button.textContent.trim());
-            button.classList.toggle('checked');
+            await navigator.clipboard.writeText(pre.textContent.trim());
+            button.classList.add('checked');
             setTimeout(() => {
-                button.classList.toggle('checked');
+                button.classList.remove('checked');
             }, 1000);
-
         } catch (err) {
             console.error('Failed to copy: ', err);
         }
