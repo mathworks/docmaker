e7751e48d60e43ae299c855463758deeef5f0070
David Sampson <dsampson@mathworks.com>
2024-10-14 08:45:50 +0100

    Renamed GitHub token secret

--- tbx/docer/+docer/md2xml.m
+++ tbx/docer/+docer/md2xml.m
@@ -22,9 +22,9 @@ request = addFields( request, "Content-Type", "text/plain" );
 if ~isempty( getenv( "DOCER_GITHUB_TOKEN" ) )
     request = addFields( request, "Authorization", ...
         "Bearer " + getenv("DOCER_GITHUB_TOKEN"));
-elseif ~verLessThan( "MATLAB", "24.1" ) && isSecret( "GitHub API token" ) %#ok<VERLESSMATLAB>
+elseif ~verLessThan( "MATLAB", "24.1" ) && isSecret( "Doc'er GitHub token" ) %#ok<VERLESSMATLAB>
     request = addFields( request, "Authorization", ...
-        "Bearer " + getSecret( "GitHub API token" ) );
+        "Bearer " + getSecret( "Doc'er GitHub token" ) );
 elseif ispref( "docer", "token" )
     request = addFields( request, "Authorization", ...
         "Bearer " + getpref( "docer", "token" ) );
