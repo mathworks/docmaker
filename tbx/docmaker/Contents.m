18772cff6e37dce838a04b2513c2ff28ba2ada68
David Sampson <dsampson@mathworks.com>
2026-01-11 13:37:04 +0000

    Bumped release date

--- tbx/docmaker/Contents.m
+++ tbx/docmaker/Contents.m
@@ -1,5 +1,5 @@
 % MATLAB DocMaker
-% Version 0.6 (R2025b) 09-Jan-2026
+% Version 0.6 (R2025b) 11-Jan-2026
 %
 % docconvert - convert Markdown documents to HTML
 % docrun     - run MATLAB code in HTML documents and insert output
